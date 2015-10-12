0284427e1cd9e0b937df59b0d3e5e654e79f7cd9
Adrian P. Blunier <adrian@blunier.net>
2015-10-12 20:00:51 +0200

    updated errors alert

--- views/atoms/alerts/errors.blade.php
+++ views/atoms/alerts/errors.blade.php
@@ -1,10 +1,8 @@
 @if (count($errors) > 0)
-    @foreach ($errors->all() as $error)
-        @include ('adoadomin::atoms.alerts.default', [
-            'type' => 'danger',
-            'icon' => 'fa-exclamation-circle',
-            'title' => trans('adoadomin::messages.validation_error_title'),
-            'text' => $error
-        ])
-    @endforeach
+    @include ('adoadomin::atoms.alerts.default', [
+        'type' => 'danger',
+        'icon' => 'fa-exclamation-circle',
+        'title' => trans('adoadomin::messages.validation_error_title'),
+        'text' => implode(' ', $errors->all())
+    ])
 @endif
