80c1a228ce29d1feea10ae14b58f7c2792d6a79d
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2015-10-14 09:04:45 +0200

    added knp menu

---
+++ src/View/Menu/SidebarMenuProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace ANavallaSuiza\Adoadomin\View\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\Renderer\RendererInterface;
+use Knp\Menu\Matcher\MatcherInterface;
+
+abstract class SidebarMenuProvider
+{
+    protected $factory;
+    protected $renderer;
+    protected $matcher;
+
+    public function __construct(FactoryInterface $factory, RendererInterface $renderer, MatcherInterface $matcher)
+    {
+        $this->factory = $factory;
+        $this->renderer = $renderer;
+        $this->matcher = $matcher;
+    }
+
+    abstract public function render();
+}
