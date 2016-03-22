2167df1fdac49e845742fa86aa6db60c13d6b776
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-03-22 16:59:52 +0100

    ckeditor with embeds

---
+++ src/Console/Commands/CreateUser.php
@@ -0,0 +1,42 @@
+<?php
+namespace Anavel\Foundation\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'anavel:user:create
+                            {email : User email}
+                            {password : User password}
+                            {name : User name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new User';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+}
