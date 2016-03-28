03320803e7eef3f8cbbad8ccf91091b9cfb93d85
Carlos Morales <carlos@cmorales.es>
2016-03-28 17:06:22 +0200

    Added AuthorizeRequest trait

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -1,11 +1,12 @@
 <?php
 namespace Anavel\Foundation\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller extends BaseController
 {
-    use DispatchesCommands, ValidatesRequests;
+    use AuthorizesRequests, DispatchesCommands, ValidatesRequests;
 }
